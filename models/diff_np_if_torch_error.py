92095449aa3b8f6b095e0e22d72bd0300cb8b1aa
Junwon Ko <72448556+koooooooook@users.noreply.github.com>
2024-12-27 18:48:20 +0900

    Update diff_np_if_torch_error.py

--- models/diff_np_if_torch_error.py
+++ models/diff_np_if_torch_error.py
@@ -23,7 +23,7 @@ import open3d as o3d
 # constants
 ModelPrediction =  namedtuple('ModelPrediction', ['pred_noise', 'pred_x_start'])
 
-
+# .
 class DiffusionModel(nn.Module):
     def __init__(
         self,
